f3e656d9b3ea2c629f6df7ab062e213f9927646b
Lukas Alexandre <lukeskytm@gmail.com>
2012-05-31 03:22:27 -0300

    Namespacing the script

--- fader.js
+++ fader.js
@@ -5,50 +5,48 @@
 * Licensed under MIT
 */
 
-function fadeOutWithId(id, time) {
-  fadeWithId(id, time, 100, 0);
-}
-function fadeInWithId(id, time) {
-  fadeWithId(id, time, 0, 100);
-}
-
-function fadeOutWithClass(className, index, time) {
-  fadeWithClass(className, index, time, 100, 0);
-}
-function fadeInWithClass(className, index, time) {
-  fadeWithClass(className, index, time, 0, 100);
-}
-
-function fadeWithId(id, time, ini, fin) {
-  var target = document.getElementById(id);
-  exec_fade(target, time, ini, fin);
-}
-function fadeWithClass(className, index, time, ini, fin) {
-  var target = document.getElementsByClassName(className)[index];
-  executeFade(target, time, ini, fin);
-}
-
-function executeFade(target, time, ini, fin) {
-  var alpha = ini;
-  var inc;
-  if (fin >= ini) {
-      inc = 2;
-  } else {
-      inc = -2;
+var Fader = {
+  fadeOutWithId: function(id, time) {
+    Fader.fadeWithId(id, time, 100, 0);
+  },
+  fadeInWithId: function(id, time) {
+    Fader.fadeWithId(id, time, 0, 100);
+  },
+  fadeOutWithClass: function(className, index, time) {
+    Fader.fadeWithClass(className, index, time, 100, 0);
+  },
+  fadeInWithClass: function(className, index, time) {
+    Fader.fadeWithClass(className, index, time, 0, 100);
+  },
+  fadeWithId: function(id, time, ini, fin) {
+    var target = document.getElementById(id);
+    Fader.exec_fade(target, time, ini, fin);
+  },
+  fadeWithClass: function(className, index, time, ini, fin) {
+    var target = document.getElementsByClassName(className)[index];
+    Fader.executeFade(target, time, ini, fin);
+  },
+  executeFade: function(target, time, ini, fin) {
+    var alpha = ini;
+    var inc;
+    if (fin >= ini) {
+        inc = 2;
+    } else {
+        inc = -2;
+    }
+    var timer = (time * 1000) / 50;
+    var i = setInterval(
+      function() {
+        if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
+          clearInterval(i);
+        }
+        Fader.setAlpha(target, alpha);
+        alpha += inc;
+      }, timer
+    );
+  },
+  setAlpha: function(target, alpha) {
+    target.style.filter = "alpha(opacity="+ alpha +")";
+    target.style.opacity = alpha/100;
   }
-  var timer = (time * 1000) / 50;
-  var i = setInterval(
-    function() {
-      if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
-        clearInterval(i);
-      }
-      setAlpha(target, alpha);
-      alpha += inc;
-    }, timer
-  );
-}
-
-function setAlpha(target, alpha) {
-  target.style.filter = "alpha(opacity="+ alpha +")";
-  target.style.opacity = alpha/100;
 }
