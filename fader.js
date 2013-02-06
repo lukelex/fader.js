9ea62f22c52022988493ef8f3a4b1fcff0170f6d
makovich <makovich@great.pm>
2013-02-06 22:43:28 +0300

    Add support for callbacks after fade effect
    Change signature of all public Fader methods.

--- fader.js
+++ fader.js
@@ -1,5 +1,5 @@
 /*!
-* Fader.js 0.3.1
+* Fader.js 0.3.2
 *
 * Copyright 2012, Lukas Alexandre, Tait Brown
 * Licensed under MIT
@@ -14,40 +14,42 @@ var Fader = {
     }
     return false;
   }()),
-  fadeOutWithId: function(id, time) {
-    Fader.fadeWithId(id, time, 100, 0);
+  fadeOutWithId: function(id, time, callback) {
+    Fader.fadeWithId(id, time, 100, 0, callback);
   },
-  fadeInWithId: function(id, time) {
-    Fader.fadeWithId(id, time, 0, 100);
+  fadeInWithId: function(id, time, callback) {
+    Fader.fadeWithId(id, time, 0, 100, callback);
   },
-  fadeOutWithClass: function(className, index, time) {
-    Fader.fadeWithClass(className, index, time, 100, 0);
+  fadeOutWithClass: function(className, index, time, callback) {
+    Fader.fadeWithClass(className, index, time, 100, 0, callback);
   },
-  fadeInWithClass: function(className, index, time) {
-    Fader.fadeWithClass(className, index, time, 0, 100);
+  fadeInWithClass: function(className, index, time, callback) {
+    Fader.fadeWithClass(className, index, time, 0, 100, callback);
   },
-  fadeOutElement: function(elem, time) {
-    Fader.fadeElement(elem, time, 100, 0);
+  fadeOutElement: function(elem, time, callback) {
+    Fader.fadeElement(elem, time, 100, 0, callback);
   },
-  fadeInElement: function(elem, time) {
-    Fader.fadeElement(elem, time, 0, 100);
+  fadeInElement: function(elem, time, callback) {
+    Fader.fadeElement(elem, time, 0, 100, callback);
   },
-  fadeWithId: function(id, time, ini, fin) {
+  fadeWithId: function(id, time, ini, fin, callback) {
     var target = document.getElementById(id);
-    Fader.executeFade(target, time, ini, fin);
+    Fader.executeFade(target, time, ini, fin, callback);
   },
-  fadeWithClass: function(className, index, time, ini, fin) {
+  fadeWithClass: function(className, index, time, ini, fin, callback) {
     var target = document.getElementsByClassName(className)[index];
-    Fader.executeFade(target, time, ini, fin);
+    Fader.executeFade(target, time, ini, fin, callback);
   },
-  fadeElement: function(elem, time, ini, fin) {
-    Fader.executeFade(elem, time, ini, fin);
+  fadeElement: function(elem, time, ini, fin, callback) {
+    Fader.executeFade(elem, time, ini, fin, callback);
   },
-  executeFade: function(target, time, ini, fin) {
+  executeFade: function(target, time, ini, fin, callback) {
+    var callback = callback || function(){};
     if (Fader.supportsTransitions) {
       target.style.opacity = (ini === 100) ? 1 : 0;
       target.style[Fader.supportsTransitions] = "opacity " + time * 1000 + "ms linear";
       target.style.opacity = (fin === 100) ? 1 : 0;
+      setTimeout(callback, time*1000);
       return;
     }
     var alpha = ini;
@@ -62,6 +64,7 @@ var Fader = {
       function() {
         if ((inc > 0 && alpha >= fin) || (inc < 0 && alpha <= fin)) {
           clearInterval(i);
+          setTimeout(callback, timer);
         }
         Fader.setAlpha(target, alpha);
         alpha += inc;
